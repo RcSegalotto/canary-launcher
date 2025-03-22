31222b8b4f1945612ee030d5a7f4053cefa4606d
BrisaOt <154487417+RcSegalotto@users.noreply.github.com>
2025-03-22 14:49:31 -0300

    Update MainWindow.xaml.cs

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class MainWindow : Window
 	{
-		static string launcerConfigUrl = "https://raw.githubusercontent.com/RCsegalotto-patch-1/canary-launcher/main/launcher_config.json";
+		static string launcerConfigUrl = "https://github.com/RcSegalotto/canary-launcher/tree/RcSegalotto-patch-1/launcher_config.json";";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
